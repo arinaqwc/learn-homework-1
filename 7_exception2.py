8b6e766a8e8c6a5546511b19c2dd196ec3eed9fb
arinaqwc <arinaalekseeva2008@mail.ru>
2025-03-09 20:26:57 +0300

    Почти вся домашняя работа

--- 7_exception2.py
+++ 7_exception2.py
@@ -1,28 +1,14 @@
-"""
+def get_summ(num_one, num_two):
+  try:
+    num_one=int(num_one)
+    num_two=int(num_two)
+    return num_one+num_two
 
-Домашнее задание №1
-
-Исключения: приведение типов
-
-* Перепишите функцию discounted(price, discount, max_discount=20)
-  из урока про функции так, чтобы она перехватывала исключения,
-  когда переданы некорректные аргументы.
-* Первые два нужно приводить к вещественному числу при помощи float(),
-  а третий - к целому при помощи int() и перехватывать исключения
-  ValueError и TypeError, если приведение типов не сработало.
-    
-"""
-
-def discounted(price, discount, max_discount=20)
-    """
-    Замените pass на ваш код
-    """
-    pass
-    
-if __name__ == "__main__":
-    print(discounted(100, 2))
-    print(discounted(100, "3"))
-    print(discounted("100", "4.5"))
-    print(discounted("five", 5))
-    print(discounted("сто", "десять"))
-    print(discounted(100.0, 5, "10"))
+  except ValueError:
+    return "Введите числа"
+  
+print(get_summ(2, 2))
+print(get_summ(3, "3"))
+print(get_summ("4", "4"))
+print(get_summ("five", 5))
+print(get_summ("six", "шесть"))
